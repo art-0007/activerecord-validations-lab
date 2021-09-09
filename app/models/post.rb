6cc81d8e67015942905994693bca3beaf4844c5d
Artur <info@aigroupusa.com>
2021-09-09 13:28:09 -0400

    commit tree 87d9c33fae31f54f668cd689fc4c29a3dc30da25

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction ), message: "not a valid category" }
+    validate :clickbait
+
+    def clickbait
+        return false if title == nil
+        unless ["Won't Believe", "Secret", "Top [number]", "Guess"].any?{|el| title.match(el) }
+          errors.add(title, "title is not clickbait-y")  
+        end
+    end
 end
